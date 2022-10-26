c99140149c5428c3068acd14defa2ab5cbf6b746
junio Santos <junio.js977@gmail.com>
2022-10-25 23:01:14 -0300

    primeira pratica

---
+++ praticasflutter01/android/app/src/main/kotlin/com/example/praticasflutter01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.praticasflutter01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ praticasflutter01/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Container(
+    child: Center(
+       child: Text('ola, mundo', textDirection: TextDirection.ltr,
+       style: TextStyle(color: Colors.blue, fontSize: 20.0),
+    )
+    
+    ),
+  ));
+}
+
